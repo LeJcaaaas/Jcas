a0c500c396bb3e87e72853735dbc8803ca187e28
Richard Jarry <richard@ProBook430.home>
2018-09-29 10:20:13 +0200

    Initialisation de la Passe 2.

--- ProjetCompil/src/fr/esisar/compilation/verif/ErreurContext.java
+++ ProjetCompil/src/fr/esisar/compilation/verif/ErreurContext.java
@@ -1,26 +1,50 @@
 /**
  * Type énuméré pour les erreurs contextuelles.
- * Ce type énuméré définit toutes les erreurs contextuelles possibles et 
+ * Ce type énuméré définit toutes les erreurs contextuelles et 
  * permet l'affichage des messages d'erreurs pour la passe 2.
  */
-
-// -------------------------------------------------------------------------
-// A COMPLETER, avec les différents types d'erreur et les messages d'erreurs 
-// correspondants
-// -------------------------------------------------------------------------
 
 package fr.esisar.compilation.verif;
 
 public enum ErreurContext {
-   
-   ErreurNonRepertoriee;
 
-   void leverErreurContext(String s, int numLigne) throws ErreurVerif {
+   NameError,		// IDF est un mot-clef
+   EnvtError,		// IDF jamais ete declare
+   LookupError,		// Erreur de redeclaration
+   IndexError,		// Erreur de plages numeriques
+   ValueError,		// Incoherence d'affectation
+   TypeError,		// Incoherence de types
+
+   UnknownError,	// Erreur inconnue (default?)
+   
+   /* Bonus */
+   OverflowError,	// Depassement de valeur (int)
+   ZeroDivError;	// Division par zero
+
+
+   void leverErreurContext(String s, int numLigne) throws ErreurVerif
+   {
+      String msg = "";
       System.err.println("Erreur contextuelle : ");
-      switch (this) {
-         default:
-            System.err.print("non repertoriee");
+      
+      switch (this)
+      {
+         case NameError:	msg = "Identificateur reserve"; break;
+         case EnvtError:	msg = "Identificateur inconnu";	break;
+         case LookupError:	msg = "Identificateur declare";	break;
+         case IndexError:	msg = "Plage num. incorrecte";	break;
+         case ValueError:	msg = "Erreur d'affectation";	break;
+         case TypeError:	msg = "Types incompatibles";	break;
+         
+         case OverflowError:	msg = "Depassement d'entier";	break;
+         case ZeroDivError:	msg = "Operation interdite";	break;
+         
+         case UnknownError:	msg = "Erreur inconnue";	break;
+         
+         default:		msg = "Erreur non repertoriee"; break;
       }
+      
+      System.err.print(s + " : "+ msg);
       System.err.println(" ... ligne " + numLigne);
       throw new ErreurVerif();
    }
--- Readme.md
+++ Readme.md
@@ -10,6 +10,8 @@ Ce générateur a été pensé pour pouvoir générer des programmes simples, sy
 
 Le compilateur quant à lui est composé d'une série de classes Java, interagissant avec des scripts shell et des fichiers de description de la grammaire et du lexique, respectivement ``syntax.cup`` et ``lexical.flex``. Ces fichiers, complétés au cours de la passe 1, sont documentés par leur spécification disponible sur la plate-forme Chamilo, et leur étude a été consignée dans ``lexical.doc.md`` et ``syntaxe.doc.md``, dans le même répertoire.
 
+Dans la passe 2, nous nous intéressons à la décoration de l'arbre abstrait généré à l'étape précédente, ainsi qu'à sa correction sémantique. Nous complétons à cet effet les fichiers ``Verif.java``, ``ErreurContext.java``, ``ReglesTypage.java`` et ``ErreurReglesTypage.java`` dans le répertoire correspondant.
+
 ## 2. Fonctionnement des programmes
 
 Pour créer de nouveaux fichiers de test (si besoin) :
@@ -23,7 +25,8 @@ Pour compiler le programme de compilation jcas :
 ```Bash
 chmod -R u+x ProjetCompil/
 
-cd ProjetCompil/src/fr/esisar/compilation/syntaxe/
+cd ProjetCompil/src/fr/esisar/compilation/syntaxe/	# Passe 1
+cd ProjetCompil/src/fr/esisar/compilation/verif/	# Passe 2
 
 ./compil.sh
 ```
